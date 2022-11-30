5bec516bd15093abc5789394a0b84cfbed3cb6e9
samandar <xkas2001@gmail.com>
2022-11-30 19:52:39 +0500

    Practice with Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return "Bu userlarni royxati";
+    }
+
+    public function show($user)
+    {
+        return "tanlangan user: " . $user;
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function edit($user_id)
+    {
+        return $user_id.' ni o\'uzgartirish';
+    }
 }
