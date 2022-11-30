c8e2d37cdcdd5e635ac028f22d6740421e659a63
samandar <xkas2001@gmail.com>
2022-12-01 00:42:24 +0500

    Lesson 12 Practise

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,12 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'main']);
 
-Route::get('/users', [UserController::class, 'index']);
+
+
+
+/*Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/create', [UserController::class, 'create']);
 Route::get('/users/{user}', [UserController::class, 'show']);
 Route::get('/users/{user}/edit', [UserController::class, 'edit']);
-Route::post('/user-create', [UserController::class, 'store']);
+Route::post('/user-create', [UserController::class, 'store']);*/
