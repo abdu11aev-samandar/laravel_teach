95686d41bfc5777a6013f8ec2896c9718e7916c3
samandar <xkas2001@gmail.com>
2022-12-06 09:31:32 +0500

    Lesson 18 Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,7 +14,41 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+
+        /*$newPost = new Post;
+        $newPost->title = 'new posts';
+        $newPost->short_content = 'new post short content';
+        $newPost->content = 'new post content';
+        $newPost->photo = '/storage/new_post.png';
+
+        $newPost->save();*/
+
+        /*$newPost = Post::create([
+            'title' => '5',
+            'short_content' => 'short',
+            'content' => 'content 123',
+            'photo' => 'photo.jpg'
+        ]);*/
+
+        /*$post = Post::find(4);
+        $post->title = "O'zgartirilgan sarlavha";
+        $post->save();*/
+
+//        $post = Post::find(4)->update(['title' => "2 O'zgartirilgan sarlavha"]);
+
+//        $post = Post::find(5);
+//        $post->delete();
+
+//        Post::destroy(1);
+
+        Post::withTrashed()->find(1)->restore();
+
+//        Post::destroy(1);
+        $posts = Post::all();
+        dd($posts);
+
+//        return view('posts.index');
+        return 'success deleted';
     }
 
     /**
--- database/migrations/2022_12_06_031304_create_comments_table.php
+++ database/migrations/2022_12_06_031304_create_comments_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -12,14 +13,9 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->text('title');
-            $table->text('short_content');
-            $table->text('content');
-            $table->string('photo')->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -30,6 +26,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
