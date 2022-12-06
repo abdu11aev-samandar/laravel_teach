8c9968d472c14c771140c91149bc1734df7690c9
samandar <xkas2001@gmail.com>
2022-12-06 20:23:07 +0500

    Lesson 21 Create Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
     public function index()
     {
         $posts = Post::all();
@@ -20,33 +15,22 @@ class PostController extends Controller
         return view('posts.index')->with('posts', $posts);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $post = Post::create([
+            'title' => $request->title,
+            'short_content' => $request->short_content,
+            'content' => $request->content,
+        ]);
+
+        return redirect()->route('posts.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
     public function show(Post $post)
     {
         return view('posts.show')->with([
@@ -55,35 +39,16 @@ class PostController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,10 +1,47 @@
 <x-layouts.main>
-    <x-slot:title>
 
+    <x-slot:title>
+        Post Yaratish
     </x-slot:title>
 
     <x-page-header>
-
+        Yangi Post Yaratish
     </x-page-header>
+
+    <div class="container">
+        <div class="w-50 py-4">
+            <div class="contact-form">
+                <div id="success"></div>
+                <form action="{{ route('posts.store') }}" method="post">
+                    @csrf
+                    <div class="control-group">
+                        <input type="text" class="form-control p-4" name="title" placeholder="Sarlavha"
+                               required="required"/>
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    {{--<div class="control-group">
+                        <input type="file" class="form-control p-4" id="subject" placeholder="Rasm"/>
+                        <p class="help-block text-danger"></p>
+                    </div>--}}
+                    <div class="control-group">
+                                <textarea class="form-control p-4" rows="3" name="short_content"
+                                          placeholder="Qisqacha Ma'lumot"
+                                          required="required"></textarea>
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div class="control-group">
+                                <textarea class="form-control p-4" rows="6" name="content" placeholder="Maqola"
+                                          required="required"></textarea>
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div>
+                        <button class="btn btn-primary btn-block py-3 px-5" type="submit">
+                            Saqlash
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 </x-layouts.main>
