4b22b52b0910ce449c8f57d02f122515a11665d7
samandar <xkas2001@gmail.com>
2022-12-07 11:12:55 +0500

    Lesson 26 Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::latest()->paginate(3);
 
         return view('posts.index')->with('posts', $posts);
     }
