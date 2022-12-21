1eca055f72cf58de6c437a38bca0265884ce8abc
samandar <xkas2001@gmail.com>
2022-12-21 10:04:33 +0500

    Changed in posts index blade file

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -54,10 +54,10 @@
                         <h5 class="font-weight-medium mb-2">{{ $post->title }}</h5>
                         <p class="mb-4">{{ $post->short_content }}</p>
                         {{--@if(auth()->user()->role->name == 'admin')--}}
-                        @if(auth()->user()->hasRole('blogger'))
+                        {{--@if(auth()->user()->hasRole('blogger'))--}}
                             <a class="btn btn-sm btn-primary py-2"
                                href="{{ route('posts.show',['post' => $post->id ]) }}">O'qish</a>
-                        @endif
+                        {{--@endif--}}
                     </div>
                 @endforeach
 
