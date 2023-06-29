b0f31bfec14faf74f94f3004abdd13609ad25ce9
xkas01 <xkas2001@gmail.com>
2023-06-29 11:59:55 +0500

    lesson 37 Events and Listeners

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\PostCreated;
 use App\Http\Requests\StorePostRequest;
 use App\Models\Category;
 use App\Models\Post;
@@ -73,6 +74,8 @@ class PostController extends Controller
                 $post->tags()->attach($tag);
             }
         }
+
+        PostCreated::dispatch($post);
 
         return redirect()->route('posts.index');
     }
